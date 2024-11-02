a372ec7f8f0f31da6e98556a0f7bd39cb96a3a42
quikiet <quikiet03@gmail.com>
2024-11-02 11:03:10 +0700

    hahaha

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,21 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
+int tich(int a, int b) {
+	return a * b;
+}
+float thuong(int a, int b) {
+	return a / b;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 
+
+	cout << "Tich là: " << tich(5, 2) << endl;
+	cout << "Thương là: " << tich(62,42)<<endl;
 	system("pause");
 	return 0;
-}
+}	
